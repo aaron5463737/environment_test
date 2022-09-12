fd72a9171b1e1eeccebbf09a1df6cc43795c0f59
aaron54637 <aaron5463737@gmail.com>
2022-09-11 19:35:17 -0500

    Lab3Heroku

--- test_app/test/models/book_test.rb
+++ test_app/test/models/book_test.rb
@@ -4,4 +4,26 @@ class BookTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+class MyUnitTests < Test::Unit::TestCase    
+  def test_title
+    title = 
+    assert_equal("Ruby on Rails", title)
+  end
+
+  def test_author
+    author = 
+    assert_equal("Aaron Calderon", author)
+  end
+
+  def test_price
+    price = 
+    assert_equal("10", price)
+  end
+
+  def test_date
+    published_date = 
+    assert_equal("09/06/22", published_date)
+  end
+
 end
